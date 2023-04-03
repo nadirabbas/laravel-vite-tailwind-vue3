ce8b605f3669f6f34df7c70f180e220713a4243a
Nadir Abbas <nadirabbasforwork@gmail.com>
2023-04-03 21:39:16 +0500

    add tailwind, vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
+import path from "path";
 
 export default defineConfig({
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/app/main.ts"],
+            postcss: [tailwindcss(), autoprefixer()],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "resources/app"),
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@import "@/assets/styles/_variables.scss";`,
+            },
+        },
+    },
 });
